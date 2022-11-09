585e91321f733904ac9fc040c41542ac67398362
AaronVenema <34764586+AaronVenema@users.noreply.github.com>
2022-11-08 19:40:22 -0600

    final commit

--- src/html.js
+++ src/html.js
@@ -9,14 +9,14 @@ const generatePage = function (teamProfile) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
-<body>
-  <div>
+<body class="bg-success text-white">
+  <div class="container-fluid bg-success">
     <div>
-      <h1>My Team</h1>
+      <h1 class="display-3 fw-bold text-center">My Team</h1>
     </div>
   </div>
-  <div>
-    <div>
+  <div class="container-body">
+    <div class="row">
       ${teamProfile}
     </div>
   <div>
@@ -31,31 +31,31 @@ const generateProfile = function (array) {
   if (array.title === "manager") {
     roleIcon = `<i class="fas fa-mug-hot"></i>`
     roleInfo = `Office Number: ${array.officeNumber}`
-} else if (array.title === "engineer") {
+  } else if (array.title === "engineer") {
     roleIcon = `<i class="fas fa-glasses"></i>`
-    roleInfo = `GitHub Username: <a href="https://github.com/${array.github}" target="_blank">${array.github}</a>`
-} else if (array.title === "intern") {
+    roleInfo = `GitHub Username: <a class="text-white" href="https://github.com/${array.github}" target="_blank">${array.github}</a>`
+  } else if (array.title === "intern") {
     roleIcon = `<i class="fas fa-user-graduate"></i>`
     roleInfo = `School: ${array.school}`
-}
+  }
 
-return `
-<div>
-  <div>
-    <div>
-      <h2>
+  return `
+<div class="col-md-2 m-4">
+  <div class="card bg-secondary">
+    <div class="card-header">
+      <h2 class="text-center">
         ${array.name}
       </h2>
-      <h2>
+      <h2 class="text-center">
         ${roleIcon} ${array.title}
       </h2>
     </div>
-    <div>
-      <ul>
+    <div class="text-center">
+      <ul class="list-unstyled">
         <li> 
         Employee ID: ${array.id}
         </li>
-        <li> Email: <a href="mailto:${array.email}">${array.email}</a>
+        <li> Email: <a class="text-white" href="mailto:${array.email}">${array.email}</a>
         </li>
         <li>${roleInfo}
         </li>
@@ -66,4 +66,4 @@ return `
 }
 
 exports.generatePage = generatePage
-exports.generateProfile =generateProfile
+exports.generateProfile = generateProfile
